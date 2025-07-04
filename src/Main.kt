977fc1b19785dbcc28b0714ea0646b0332a2c683
GFO <gia.focxverashvili@gmail.com>
2025-07-04 10:20:54 +0400

    final

--- src/Main.kt
+++ src/Main.kt
@@ -2,45 +2,37 @@ fun main() {
     val s = "III"
     val s2 = "MCMXCIV"
 
-    println(romanToInt(s))
+    println(romanToInt(s2))
 }
 
 fun romanToInt(s: String): Int {
-    var currentValue: Int
-    var nextValue: Int
-
-    var currentSymbol: String
-    var nextSymbol: String
+    val romanValues: Map<Char, Int> = mapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+    )
 
     var result = 0
     var i = 0
 
-    while (i < s.length - 1) {//"III"
+    while (i < s.length) {
+        val current = romanValues[s[i]] ?: 0
+        val next = if (i + 1 < s.length) romanValues[s[i + 1]] ?: 0 else 0
 
-        currentSymbol = s[i].toString() // M
-        nextSymbol = s[i + 1].toString() // X
-
-        currentValue = romanValues[currentSymbol] ?: 0
-        nextValue = romanValues[nextSymbol] ?: 0
-
-        if (currentValue < nextValue) {
-            result += nextValue - currentValue
-            i++
+        if (current < next) {
+            result += next - current
+            i += 2
         } else {
-            result += currentValue
+            result += current
+            i++
         }
-        i++
     }
     return result
 }
 
-val romanValues = mapOf(
-    "I" to 1,
-    "V" to 5,
-    "X" to 10,
-    "L" to 50,
-    "C" to 100,
-    "D" to 500,
-    "M" to 1000
-)
+
 
